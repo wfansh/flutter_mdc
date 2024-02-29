8cbd6a185d05dc2240ea7fa8663d30a80051824f
Fan Wang <wfansh@google.com>
2024-02-29 12:57:09 +0800

    Add files via upload

---
+++ basics/lib/login.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class LoginPane extends StatefulWidget {
+  const LoginPane({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPane> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            const SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Image.asset('assets/diamond.png'),
+                const SizedBox(height: 16.0),
+                const Text('SHRINE'),
+              ],
+            ),
+            const SizedBox(
+              height: 120.0,
+            ),
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration:
+                  const InputDecoration(filled: true, labelText: 'Password'),
+              obscureText: true,
+            ),
+            OverflowBar(
+              alignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('CANCEL'),
+                  onPressed: () {
+                    _usernameController.clear();
+                    _passwordController.clear();
+                  },
+                ),
+                ElevatedButton(
+                  child: const Text('NEXT'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- basics/lib/main.dart
+++ basics/lib/main.dart
@@ -1,1 +1,4 @@
+import 'package:flutter/material.dart';
+import 'app.dart';
 
+void main() => runApp(const ShrineApp());
